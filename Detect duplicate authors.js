39f471ffac5670356e60a78eb0ae658f9c428d19
Adam Horvath <info@info>
2018-12-21 14:44:54 +1100

    Initial commit

---
+++ Detect duplicate authors.js
@@ -0,0 +1,98 @@
+{
+  "translatorID": "08dfa05d-3b55-47d5-8a05-c43cb6a1ae97",
+  "label": "Detect duplicate authors across papers",
+  "description": "Detect potentially misspelled but same authors across papers",
+  "creator": "Adam Horvath",
+  "target": "txt",
+  "minVersion": "4.0.27",
+  "maxVersion": "",
+  "configOptions": {
+    "getCollections": false
+  },
+  "displayOptions": {
+    "exportFileData": false
+  },
+  "translatorType": 2,
+  "browserSupport": "gcsv",
+  "priority": 100,
+  "inRepository": false,
+  "lastUpdated": "2018-12-21 15:58:41"
+}
+
+var levenshtein = function(a, b) {
+    if (a.length == 0) return b.length;
+    if (b.length == 0) return a.length;
+
+    // swap to save some memory O(min(a,b)) instead of O(a)
+    if (a.length > b.length) {
+        var tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    var row = [];
+    // init the row
+    for (var i = 0; i <= a.length; i++) {
+        row[i] = i;
+    }
+
+    // fill in the rest
+    for (var i = 1; i <= b.length; i++) {
+        var prev = i;
+        for (var j = 1; j <= a.length; j++) {
+            var val;
+            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                val = row[j - 1]; // match
+            } else {
+                val = Math.min(row[j - 1] + 1, // substitution
+                    prev + 1,     // insertion
+                    row[j] + 1);  // deletion
+            }
+            row[j - 1] = prev;
+            prev = val;
+        }
+        row[a.length] = prev;
+    }
+
+    return row[a.length];
+}
+
+function doExport() {
+    let items = [];
+    let item = null;
+    while (item = Zotero.nextItem()) {
+        item.creators.forEach(creator => {
+            items.push({
+                author: {
+                    firstName: creator.firstName.toLowerCase(),
+                    lastName: creator.lastName.toLowerCase()
+                },
+                title: item.title
+            });
+        });
+    }
+
+    let reported = new Set();
+    for (let i = 0; i < items.length; i++) {
+        let duplicates = [items[i]];
+        let author = items[i].author;
+
+        for (let j = i + 1; j < items.length; j++) {
+            let otherAuthor = items[j].author;
+            if (author.firstName.substr(0, 1) != otherAuthor.firstName.substr(0, 1)) {
+                continue;
+            }
+
+            let distance = levenshtein(author.lastName, otherAuthor.lastName);
+            if (distance <= 1 && !reported.has(items[j])) {
+                duplicates.push(items[j]);
+                reported.add(items[j]);
+            }
+        }
+
+        if (duplicates.length > 1) {
+            duplicates.forEach(item => Zotero.write(item.author.lastName + ', ' + item.author.firstName + ': ' + item.title + '\n'));
+            Zotero.write('\n');
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# zotero-detect-duplicate-authors
+
+When installed in Zotero, this translator will exporting the list of potentially same authors but different naming style across papers to these can be manually corrected. The idea is based on the bug report https://github.com/retorquere/zotero-better-bibtex/issues/1067
+
+**How to install**
+1.  Download the [Detect duplicate authors.js]("Detect duplicate authors.js") file
+2. Move the "Detect duplicate authors.js" file to "[User home directory]/Zotero/translators" folder 
+3. Restart Zotero 
+
+**How to run**
+1. Select all the items you want to cross check
+2. Right click, "Export Items…"
+3. Select format “Detect duplicate authors across papers", click OK, Save
+4. Check the exported txt file for duplicated authors
+
